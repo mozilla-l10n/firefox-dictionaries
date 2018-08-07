1f0b09eac0ad4f3765872bc2fd973d1be1f55da2
Francesco Lodolo [:flod] <flodolo@mozilla.com>
2018-08-07 08:41:29 +0200

    Add script to extract minimal data

---
+++ scripts/extract_minimal.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python3
+
+import json
+import os
+
+
+def main():
+    script_folder = os.path.abspath(os.path.dirname(__file__))
+
+    # Read JSON data
+    file_name = os.path.join(
+        script_folder, os.path.pardir, 'output', 'dictionaries_curated.json')
+    with open(file_name) as inputfile:
+        curated_data = json.load(inputfile)
+
+    minimal_data = {}
+    for locale_code, locale_data in curated_data.items():
+        minimal_data[locale_code] = []
+        for dictionary in locale_data:
+            minimal_data[locale_code].append(dictionary['guid'])
+
+    # Write JSON data
+    file_name = os.path.join(
+        script_folder, os.path.pardir, 'output', 'dictionaries_minimal.json')
+    with open(file_name, 'w') as outputfile:
+        json.dump(minimal_data, outputfile, indent=2, sort_keys=True)
+
+
+if __name__ == '__main__':
+    main()
